a74a1908ab3f2c1855f8a6a1dbdf9abde0f427fa
Shruti Nagpal <130729152+testgithubrites@users.noreply.github.com>
2023-04-16 14:22:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('#nav-btn').addEventListener('click', () => {
+ document.querySelector('#navbar').classList.toggle('show-nav')
+})
+
+document.querySelector('#nav-close').addEventListener('click', () => {
+ document.querySelector('#navbar').classList.toggle('show-nav')
+})
